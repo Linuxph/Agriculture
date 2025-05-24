278cbb1c3caabddeaeaded8494008a156bc83a16
Linuxph <manassinghmakhija@gmail.com>
2025-05-24 17:24:42 +0530

    responsiveness

--- scripts.js
+++ scripts.js
@@ -17,7 +17,17 @@ const products = [
 <br> \
 <br> \
 (यदि खेत में गोबर खाद नहीं डालते है, तो भूमि की उर्वरा शक्ति बनाये रखने एवं अच्छे उत्पादन के लिए पूरे वर्ष में 5-10 कि. ग्रा. प्रो कार्बन GR प्रति एकड़ फसल अनुसार अवश्य प्रयोग करें, यह जमीन में गोबर खाद की पूर्ति करेगा।)",
-  },
+
+    Engdescription:`Pro-Carbon GR is a unique type of enzymatically extracted carbon with nutrients. It can be
+applied to improve soil fertility and structure by providing organic matter and essential nutrients.
+Providing plant available nutrients such as organic Nitrogen, Organic Phosphorus, organic K,
+and micronutrients for healthy plant growth. The organic carbon content can support beneficial
+soil microorganisms, enhancing soil health and nutrient cycling.
+Pro-Carbon is commonly used in all crops to enhance root activity, including nutrient and water
+uptake in the refreshed plant root zone. During the early vegetative stage of plant growth,
+Pro-Carbon promotes the development of white roots.`,
+
+},
 
   {
     id: 2,
@@ -31,13 +41,23 @@ const products = [
 <br>\
 <br> \
 (यदि खेत में गोबर खाद नहीं डालते है, तो भूमि की उर्वरा शक्ति बनाये रखने एवं अच्छे उत्पादन के लिए पूरे वर्ष में 3-5 लीटर प्रो कार्बन प्रति एकड़ फसल अनुसार अवश्य प्रयोग करें, यह जमीन में गोबर खाद की पूर्ति करेगा।)",
-  },
+        Engdescription:`Pro-Carbon is a unique type of enzymatically extracted carbon with nutrients. It can be applied
+to improve soil fertility and structure by providing organic matter and essential nutrients.
+Providing plant available nutrients such as organic Nitrogen, Organic Phosphorus, organic K,
+and micronutrients for healthy plant growth. The organic carbon content can support beneficial
+soil microorganisms, enhancing soil health and nutrient cycling.
+Pro-Carbon is commonly used in all crops to enhance root activity, including nutrient and water
+uptake in the refreshed plant root zone. During the early vegetative stage of plant growth,
+Pro-Carbon promotes the development of white roots.`,
+
+},
   {
     id: 3,
     name: "Amino",
     title: "अमिनो (अमिनो एसिड) ",
     image: "./public/Amino.jpg",
-    description: "अमिमो अमिनो एसिड का एक अनूठा मिश्रण है, जिसमें पौधे से प्राप्त फ्री अमिनो एसिड 65 प्रतिशत है। यह सभी फसलों में उपयोग किया जाता है। इसका प्रयोग फसलों पर छिड़काव द्वारा और रूट ड्रेचिंग, ड्रिप सिंचाई दोनों ही तरीके से किया जा सकता है। \
+    description:
+      "अमिमो अमिनो एसिड का एक अनूठा मिश्रण है, जिसमें पौधे से प्राप्त फ्री अमिनो एसिड 65 प्रतिशत है। यह सभी फसलों में उपयोग किया जाता है। इसका प्रयोग फसलों पर छिड़काव द्वारा और रूट ड्रेचिंग, ड्रिप सिंचाई दोनों ही तरीके से किया जा सकता है। \
 <li> फसल बढ़वार में मदद करता है। </li>\
 <li> फसलों को तनाव मुक्त करें। </li>\
 <li> फसलों से पीलापन हटाये। </li>\
@@ -45,7 +65,12 @@ const products = [
 <li> यह  पोषक तत्वों से भरपूर अमिनो एसिड है। </li> \
 <br> \
 अमिनो की विभिन्न फसलों में सिफारिश मात्रा सभी फसलों में अमिमो की 500 मि. ली. मात्रा को 180-200 लीटर पानी में मिलाकर एक समान रूप से छिड़काव करें। केला, पपीता एवं अन्य बागवानी फसलों में प्रति 1000 पौधों पर 500 मि.ली. ड्रिप से या ड्रेचिंग करें। अन्य फसलों में 500 मि. ली. प्रति एकड़ ड्रिप या ड्रेचिंग करें।",
-  },
+        Engdescription:`Amimo is a superior product derived from natural amino acids. It contains a mixture of free amino acids that plants
+can absorb through their roots or leaves. Amimo is highly effective in promoting growth, branching, and increasing the
+number of flowers and fruit set. It is highly recommended as an organic supplement for stress management in all
+crops`,
+
+},
 
   {
     id: 4,
@@ -59,26 +84,39 @@ const products = [
 <li> फसल की कीटों से लड़ने की क्षमता बढ़ाता है। </li>\
 <br> \
 सभी फसलों में थ्रिप्स और मकड़ी के नियंत्रण के लिए हमर की 250 मि. ली. मात्रा को 180-200 लीटर पानी में मिलाकर फसल पर एक सामान रूप से छिड़काव करें। </li>",
-  },
+        Engdescription:`Hummerr is an organic insect controller that is specifically formulated to combat thrips, mites, and leaf miners in
+plants. It enhances the plant's natural immunity against these pests. This versatile product is suitable for use in all
+types of crops, making it a reliable solution for pest control. Its environmentally friendly composition ensures the
+safety of both plants and beneficial insects. Hummerr is an effective tool for protecting crops and promoting healthy
+plant growth, contributing to sustainable agricultural practices.`,
+
+},
 
   {
     id: 5,
     name: "Silistick",
     title: "सिलिस्टिक (विटामीन-C युक्त सिलिकॉन) ",
     image: "./public/Silistick.jpg",
-    description: "<li>यह विटामीन-C युक्त उच्च गुणवत्ता वाला सिलिकॉन स्प्रेडर है। </li>\
+    description:
+      "<li>यह विटामीन-C युक्त उच्च गुणवत्ता वाला सिलिकॉन स्प्रेडर है। </li>\
 <li> यह किसी भी प्रकार के इंसक्टिसाइड, फंगीसाइड, वीडीसाइड या उर्वरकों के साथ उपयोग किया जा सकता है। </li>\
 <li> यह इंसेक्टिसाइड, फंगीसाइड, वीडीसाइड या उर्वरकों को फसल की पत्तियों पर अच्छी तरह से फैलाता है साथ ही पत्तियों से अवशोषण में भी मदद करता है।</li> \
 <li> जिन फसलों में पोलीनेशन की आवश्यकता होती है, उन फसलों में पोलीनेशन को बढ़ाने के लिए मधुमक्खी को भी आकर्षित करने में मदद करता है। </li>\
 <br> \
 सभी फसलों में सिलिस्टक 60 मि. ली. मात्रा को 180-200 लीटर पानी में मिलाकर एक समान रूप से छिड़काव करें।",
-  },
+
+        Engdescription:`Silistick is a high-quality, silicon-based, non-ionic, and highly concentrated spreader cum penetrator specifically
+designed for agricultural applications. It possesses exceptional spreading and penetrating capabilities, allowing for
+efficient and rapid dispersion of agrochemicals such as pesticides, fungicides, herbicides, and fertilizers. It increase
+pollination activity in crops.`,
+},
   {
     id: 6,
     name: "ProGro 20",
     title: "प्रोग्रो 20 (अमिनो एसिड + सिवीड एक्सट्रेक्ट + फुल्वीक एसिड) ",
     image: "./public/ProGro20.jpg",
-    description: "* यह एक बायोस्टिमुलेंट है, जो फसलों में जैविक और अजैविक त नाव को कम करता है। </li>\
+    description:
+      "* यह एक बायोस्टिमुलेंट है, जो फसलों में जैविक और अजैविक त नाव को कम करता है। </li>\
 <li> यह जैव प्रो‌द्योगिकी अनुसंधान में एक नवाचार का प्रतिनिधित्व करता है। </li> \
 <li> जिसमें समुद्री शैवाल के अर्क और पौधों के अर्क से प्राप्त अमीनों एसिड के मिश्रण से निर्मित एक अनूठा फार्मूला शामिल है। </li>\
 <li> यह फसलों में हर महत्वपूर्ण अवस्था और विभिन्न परिस्थितियों में अत्यधिक प्रभावी साबित होता है। </li>\
@@ -86,39 +124,55 @@ const products = [
 <br> \
 सभी फसलों में प्रोग्रो 20 की 500 मि.ली. मात्रा को 180-200 लीटर पानी में मिलाकर एक समान रूप से छिड़काव करें। केला, पपीता एवं अन्य बागवानी फसलों में प्रति 1000 पौधों पर 500 मि.ली. ड्रिप से या ड्रेचिंग करें। \
 अन्य फसलों में 500 मि.ली. प्रति एकड़ ड्रिप या ड्रेचिंग करें।",
-  },
+  
+        Engdescription:`ProGro 20, is a plant biostimulant, reduces biotic and abiotic stress in crops. It represents an innovation in
+biotechnological research, featuring a unique formula produced by seaweed extract and a mixture of amino acids
+derived from plant extracts. It proves highly effective in crops at every critical stage and under various conditions.`,
+},
   {
     id: 7,
     name: "Set Up",
     title: "सेट अप (प्लांट प्रोटिन्स + विटामिन्स) ",
     image: "./public/SetUp.jpg",
-    description: "<li>सेट अप पौधों से निकाले गए विटामिन और प्रोटीन का एक विशेष मिश्रण है, जो फसल में फुलों की संख्या बढ़ाता है, इन्हें झड़ने से रोकता है। \
+    description:
+      "<li>सेट अप पौधों से निकाले गए विटामिन और प्रोटीन का एक विशेष मिश्रण है, जो फसल में फुलों की संख्या बढ़ाता है, इन्हें झड़ने से रोकता है। \
 <li> इसमें ऐसे एंजाइम होते हैं जो नर और मादा फलों के अनुपात बने बनाए रखते है। </li>\
 <li> सेट अप अधिक से अधिक फूलों को फलों में रूपांतरित कर, उत्पादन में वृद्धि करता है। </li>\
 <li> यह फूल और फल की गुणवत्ता सुधार करता है। </li>\
 <li> बेहतर परिणाम के लिए पहला स्प्रे प्रारंभिक फूल वाली अवस्था पर और दूसरा स्प्रे फल विकास की अवस्था पर करें। </li>\
 <br> \
 सभी फसलों में सेट अप की 100 मि. ली. को 180-200 लीटर पानी में मिलाकर एक समान रूप से छिड़काव करें।",
-  },
+  
+    Engdescription:`Set UP is a special mixture of vitamins and proteins extracted from plants, which increases the number of flowers in
+the crop and prevents them from dropping. It contains enzymes that maintain the ratio of male and female flowers.
+Set UP converts more and more flowers into fruits.`
+},
   {
     id: 8,
     name: "Ami Nutri",
     title: "एमिन्यूट्री (अमिनो एसिड + माईक्रोन्युट्रीएन्ट) ",
     image: "./public/Ami_Nutri.jpg",
-    description: "<li> एमिन्यूट्री मुक्त अमीनो एसिड और सूक्ष्म पोषक तत्वों का मिश्रण है। </li>\
+    description:
+      "<li> एमिन्यूट्री मुक्त अमीनो एसिड और सूक्ष्म पोषक तत्वों का मिश्रण है। </li>\
 <li> इसमें मौजूद अमीनो एसिड पौधे की वृद्धि को बढ़ाने में मदद करतें है। </li>\
 <li> इसमें सूक्ष्म पोषक तत्व होते है, जो पौधों में सूक्ष्म पोषक तत्वों की कमी को पूरा करते है और उत्पादन क्षमता को बढ़ाते है। </li>\
 <li> इससे पौधों में क्लोरोफिल में वृद्धि होती है, जिससे प्रकाश संश्लेषण दर में वृद्धि होती है। </li>\
 <br> \
 सभी फसलों में एमिन्यूट्री की 250 मि.ली. मात्रा को 180-200 लीटर पानी में मिलाकर एक समान रूप से छिड़काव करें। केला, पपीता एवं अन्य बागवानी फसलों में प्रति 1000 पौधों पर 250 मि.ली. ड्रिप से या ड्रेचिंग करें। \
 अन्य फसलों में 250 मि.ली. प्रति एकड़ ड्रिप या ड्रेचिंग करें।",
-  },
+  
+        Engdescription:`AmiNutri is a mixture of free amino acids and micronutrients. The amino acids present in it help increase the growth
+boost of the plant. Additionally, it contains micronutrients which fulfill the deficiency of micronutrients in plants and
+increase production capacity. This leads to an increase in chlorophyll in the plant, thereby enhancing the
+photosynthesis rate.`,
+},
   {
     id: 9,
     name: "Pro 98-H",
     title: "प्रो 98 एच (ह्युमिक एसिड + पोटेशियम ऑक्साईड) ",
     image: "./public/Pro_98H.jpg",
-    description: "<li> प्रो 98 एच मिट्टी की जल धारण क्षमता को बढ़ाता है। </li>\
+    description:
+      "<li> प्रो 98 एच मिट्टी की जल धारण क्षमता को बढ़ाता है। </li>\
 <li> यह मिट्टी की भौतिक रासायनिक और जैविक गुणवत्ता में सुधार करता है। </li>\
 <li> ये सफेद जड़ों के विकास में मदद करते हैं। </li>\
 <li> मिट्टी में हवा और पानी का सही संतुलन बनाए रखता है। </li>\
@@ -127,41 +181,63 @@ const products = [
 <br>\
 सभी फसलों में प्रो 98 एच की 500 ग्राम मात्रा को 180-200 लीटर पानी में मिलाकर एक समान रूप से छिड़काव करें। केला, पपीता एवं अन्य बागवानी फसलों में प्रति 1000 पौधों पर 500 ग्राम ड्रिप से या ड्रेचिंग करें। \
 अन्य फसलों में 500 ग्राम प्रति एकड़ ड्रिप या ड्रेचिंग करें।",
-  },
+  
+    Engdescription:`Pro 98-H increases the water-holding capacity of soil, improving its physical, chemical, and
+biological qualities. These enhancements facilitate the development of white roots and maintain
+the perfect balance of air and water in the soil. Acting as a chelating agent for nutrients, it
+enhances the quality of production. Additionally, it promotes plant growth and branching`,
+},
   {
     id: 10,
     name: "ReActive",
     title: "रिएक्टिव (बायो फंगीसाईड) ",
     image: "./public/ReActive.jpg",
-    description: "<li> रिएक्टिव जैविक कवकनाशी एक विशेष रूप से तैयार किया गया कवकनाशी है, जो किसी भी मौसम या तापमान में सक्रिय रहता है। </li>\
+    description:
+      "<li> रिएक्टिव जैविक कवकनाशी एक विशेष रूप से तैयार किया गया कवकनाशी है, जो किसी भी मौसम या तापमान में सक्रिय रहता है। </li>\
 <li> रिएक्टिव का उपयोग विभिन्न फसलों में पाउडरी मिल्ड्यू, डाउनी मिल्डयू एवं सभी प्रकार के प्रति के धब्बा रोग का नियंत्रण करने के लिए किया जाता है। </li>\
 <li> यह जड़ सड़न-गलन और बिल्ट का नियंत्रित करने में प्रभावी है। </li>\
 <li> यह बीमारियों के साथ पौधे की रोगों के प्रति सहनशीलता प्रदान करता है। </li>\
 <br> \
 सभी फसलों में रिएक्टिव की 1 किलो मात्रा को 180-200 लीटर पानी में मिलाकर एक \
 समान रूप से छिड़काव करें। केला, पपीता एवं अन्य बागवानी फसलों में प्रति 1000 पौधों पर 1 किलो ड्रिप या ड्रेचिंग करें। अन्य फसलों में 1 कि. ग्रा. प्रति एकड़ ड्रिप या ड्रेचिंग करें।",
-  },
+
+    Engdescription:`ReActive biological fungicide is a specially prepared fungicide that remains active in any season or temperature. It is
+used to control powdery mildew, downy mildew, and all types of leaf spots in various crops. Additionally, it is effective
+in controlling root rot and wilt.`,
+},
 ];
 
 const productContainer = document.getElementById("product-list");
 
 const productHTML = products
   .map(
-    (product,index) => `
-    <div class="product-card m-2 w-full bg-white shadow-md rounded-lg p-4 border-black border-2 flex justify-around gap-5 items-center ${index % 2 === 0 ? '' : 'flex-row-reverse'}">
-    <div class="text-white text-center flex flex-col items-center w-[25vw] h-[500px] ">
-      <div class="w-[350px] h-[500px] overflow-hidden rounded-lg border-2 border-red-500 mx-auto">
-        <img src="${product.image}" alt="${product.name}" 
-          class="" >
-      </div>
-        
-        <h2 class="text-2xl font-bold font-[Hind]">${product.name}</h2>
+    (product, index) => `
+    <div class="product-card ${index % 2 === 0 ? "" : "reverse"}">
+  <div class="product-image-section">
+    <div class="image-container">
+      <img src="${product.image}" alt="${product.name}">
     </div>
-    <div class="flex-1">
-        <p class="text-red-600 font-bold text-2xl">${product.title}</p>
-        <div class="product-description mt-2 border-2 border-blue-500 text-black px-4 py-2 rounded-md">
-            ${product.description}
-        </div>
+    <h2 class="product-name">${product.name}</h2>
+  </div>
+
+  <div class="product-details">
+    <p class="product-title">${product.title}</p>
+    <p class="product-name-again">${product.name}</p>
+
+    <!-- Descriptions -->
+    <div class="english-description product-description">
+      ${product.Engdescription}
+    </div>
+    <div class="hindi-description product-description hidden">
+      ${product.description}
+    </div>
+
+    <button class="language-toggle">Translate</button>
+  </div>
+</div>
+
+        
+        
     </div>
 </div>
 
@@ -171,26 +247,75 @@ const productHTML = products
 
 productContainer.innerHTML = productHTML;
 
-
 //slider
 const carousel = document.getElementById("carousel");
 const slides = document.querySelectorAll("#carousel div");
 let index = 0;
 
 function showSlide(i) {
-    index = (i + slides.length) % slides.length;
-    carousel.style.transform = `translateX(-${index * 100}%)`;
+  index = (i + slides.length) % slides.length;
+  carousel.style.transform = `translateX(-${index * 100}%)`;
 }
 
 function nextSlide() {
-    showSlide(index + 1);
+  showSlide(index + 1);
 }
 
 function prevSlide() {
-    showSlide(index - 1);
+  showSlide(index - 1);
 }
 
 document.getElementById("next").addEventListener("click", nextSlide);
 document.getElementById("prev").addEventListener("click", prevSlide);
 
 setInterval(nextSlide, 3000);
+
+//Mobile slider
+const carousel2 = document.getElementById("carousel2");
+const slides2 = document.querySelectorAll("#carousel2 div");
+let index2 = 0;
+
+function showSlide(i) {
+  index2 = (i + slides2.length) % slides2.length;
+  carousel2.style.transform = `translateX(-${index2 * 100}%)`;
+}
+
+function nextSlide() {
+  showSlide(index2 + 1);
+}
+
+function prevSlide() {
+  showSlide(index2 - 1);
+}
+
+document.getElementById("next").addEventListener("click", nextSlide);
+document.getElementById("prev").addEventListener("click", prevSlide);
+
+setInterval(nextSlide, 3000);
+
+
+//toggle
+const sliderCircle = document.getElementById("slider-circle");
+document.querySelectorAll(".language-toggle").forEach((toggleButton) => {
+  toggleButton.addEventListener("click", () => {
+    const productDiv = toggleButton.closest(".product-card");
+    const englishDesc = productDiv.querySelector(".english-description");
+    const hindiDesc = productDiv.querySelector(".hindi-description");
+
+    // Toggle visibility
+    const isEnglishVisible = !englishDesc.classList.contains("hidden");
+
+    if (isEnglishVisible) {
+      englishDesc.classList.add("hidden");
+      hindiDesc.classList.remove("hidden");
+    } else {
+      englishDesc.classList.remove("hidden");
+      hindiDesc.classList.add("hidden");
+    }
+  });
+});
+
+
+// <div class="product-description mt-2 border-2 border-blue-500 text-black px-4 py-2 rounded-md">
+//     ${product.description}
+// </div>
